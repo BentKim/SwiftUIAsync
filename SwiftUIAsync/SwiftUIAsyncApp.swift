143c99903f21fd1dbe6fae72bd03bf5d719def33
Taewoo Kim <twk0128@naver.com>
2022-03-21 13:26:46 +0900

    Initial Commit

---
+++ SwiftUIAsync/SwiftUIAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncApp.swift
+//  SwiftUIAsync
+//
+//  Created by Taewoo Kim on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
